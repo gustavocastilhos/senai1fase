78079e33d1bcc85af5a72fb366ba85f04101f106
gustavocastilhos <gusmarcast@gmail.com>
2026-05-11 21:29:22 -0300

    att no desafio1 tirei o switch e add  if, mais verificacao de restante

--- 01javascript/desafios/desafio1.js
+++ 01javascript/desafios/desafio1.js
@@ -1,29 +1,30 @@
 function desafio1() {
-let distanciaEmKm = parseFloat(prompt("Digite a distância em quilômetros:"));
-let velocidade = 300000; // velocidade da luz em metros por segundo
+    let distanciaEmKm = parseFloat(prompt("Digite a distância em quilômetros:"));
+    let velocidade = 300000; // velocidade da luz em km/s
 
-    let tempoDeViagem = distanciaEmKm / velocidade;
-    let resultado = "Tempo: " + tempoDeViagem.toFixed(2) + " segundos";
-    switch (true) {
-        case (tempoDeViagem > 31536000):
-            resultado += " ou " + (tempoDeViagem / 31536000).toFixed(2) + " anos";
-            break;
-        case (tempoDeViagem > 2592000):
-            resultado += " ou " + (tempoDeViagem / 2592000).toFixed(2) + " meses";
-            break;
-        case (tempoDeViagem > 86400):
-            resultado += " ou " + (tempoDeViagem / 86400).toFixed(2) + " dias";
-            break;
-        case (tempoDeViagem > 3600):
-            resultado += " ou " + (tempoDeViagem / 3600).toFixed(2) + " horas";
-            break;
-        case (tempoDeViagem > 60):
-            resultado += " ou " + (tempoDeViagem / 60).toFixed(2) + " minutos";
-            break;
-       
-                
+    let tempoTotal = distanciaEmKm / velocidade;
+    let resultado = "Tempo: " + tempoTotal.toFixed(2) + " segundos";
+
+    if (tempoTotal > 60) {
+        let segundosRestantes = tempoTotal;
+
+        let anos = Math.floor(segundosRestantes / 31536000);
+        segundosRestantes %= 31536000;
+
+        let dias = Math.floor(segundosRestantes / 86400);
+        segundosRestantes %= 86400;
+
+        let horas = Math.floor(segundosRestantes / 3600);
+        segundosRestantes %= 3600;
+        
+        let minutos = Math.floor(segundosRestantes / 60);
+        
+        resultado += " ou ";
+        if (anos > 0) resultado += anos + " anos, ";
+        if (dias > 0) resultado += dias + " dias, ";
+        if (horas > 0) resultado += horas + " horas ";
+        if (minutos > 0) resultado += "e " + minutos + " minutos";
     }
-        document.getElementById('resultado').innerHTML =  resultado + ".";
 
+    document.getElementById('resultado').innerHTML = resultado + ".";
 }
-    
