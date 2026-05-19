00d016d1b41aa9664ca171de8f3d0572778b8b34
GUSTAVO MARTINS CASTILHOS <gustavo_castilhos@estudante.sesisenai.org.br>
2026-05-18 21:59:25 -0300

    css cabuloso

--- 03variaveis/script.js
+++ 03variaveis/script.js
@@ -29,13 +29,20 @@ let totalVendas = 0;
 
 function registrarVenda() {
     let valor = Number(document.getElementById('inputVenda').value);
+    
     totalVendas = totalVendas + valor;
-    document.getElementById('p-total').innerHTML = totalVendas;
-    document.getElementById('inputVenda').value = "";
+    document.getElementById('totalVendido').innerHTML = 'Total de vendas: R$' + totalVendas.toFixed(2);
+    document.getElementById('listaVendas').innerHTML += 'R$' + valor.toFixed(2) + '<br>';
+    document.getElementById('inputVenda').value = '';
+    document.getElementById('inputVenda').focus();
+    
     console.log(valor);
-} // <--- A função registrarVenda termina obrigatoriamente AQUI
+}
 
 function zerarContagemVendas() {
-    totalVendas = 0; 
-    document.getElementById("p-total").innerHTML = totalVendas; 
+    total = 0; 
+    
+    document.getElementById('totalVendido').innerHTML = 'Total de vendas: R$ 0.00';
+    
+    document.getElementById('listaVendas').innerHTML = ''; 
 }
