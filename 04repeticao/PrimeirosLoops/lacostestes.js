eb6d0973a8c0ef9c0c436400e58adc3df903aacb
gustavocastilhos <gusmarcast@gmail.com>
2026-06-16 20:55:31 -0300

    Feat: Primeiros loops

---
+++ 04repeticao/PrimeirosLoops/lacostestes.js
@@ -0,0 +1,18 @@
+
+// Laço que vai do número 1 até o 10
+for (let i = 1; i <= 10; i++) {
+  // Calcula o quadrado usando o operador de exponenciação (**)
+  const quadrado = i ** 2;
+  
+  // Exibe o resultado no console
+  console.log(`O quadrado de ${i} é ${quadrado}`);
+}
+
+let numero = 11
+
+while (numero > 0){
+    numero = numero - 1
+   console.log(numero)
+}
+
+
--- 04repeticao/script.js
+++ 04repeticao/script.js
@@ -1,7 +1,18 @@
-let contador = 0
+let i = 0; // inicialização
+let total = 0;
 
+while (i < 4) { // condição
+    i++; // contagem
+    let preco = Number(prompt(`Digite o preço do ${i}º produto`));
+    
+    if (isNaN(preco)) {
+        preco = 0;
+    }
+    
+    total = preco + total;
+        document.getElementById("total").innerHTML += `${i}º preço do produto: R$ ${preco} <br>`;
 
-while (contador < 10) {
- alert("Contador:" + contador)
- contador++
 }
+        document.getElementById("total").innerHTML += "O total foi de: R$" +total+  "<br>";
+
+alert(`Total da compra: R$ ${total}`);
