8b8748120dedd5c43b8660894dac4b6d16f59d55
gustavocastilhos <gusmarcast@gmail.com>
2026-06-16 21:55:32 -0300

    Feat: Primeiros arquivos loops

---
+++ 04repeticao/PrimeirosLoops/Soma1a100.js
@@ -0,0 +1,8 @@
+let numero = 0
+let soma = 0
+
+while (numero < 100){
+    numero++
+    soma += numero
+console.log(soma)
+}
---
+++ 04repeticao/PrimeirosLoops/sobedesce.js
@@ -0,0 +1,14 @@
+let numero = 11
+
+while (numero > 0){
+    numero = numero - 1
+   console.log(numero)
+}
+
+let numero2 = 0
+
+while (numero2 < 10){
+    numero2++
+    console.log(numero2)
+
+}
