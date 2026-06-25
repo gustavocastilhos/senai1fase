442635f8f04ca8040c514c882360dd7bd033e471
gustavocastilhos <gusmarcast@gmail.com>
2026-06-25 20:28:28 -0300

    Feat: first code

--- 04repeticao/relatorioDeBugs/script.js
+++ 04repeticao/relatorioDeBugs/script.js
@@ -1,5 +1,10 @@
 function relatorio(){
+let diasDeSprint = Number(prompt("Quantos dias tem o sprint? "))
 
+for(let i = 1; i <= diasDeSprint; i++){
+    let bugsEncontrados = Number(prompt("Quantos bugs foram encontrados? "))
+    document.getElementById('listaDosDias').innerHTML += "Dia " + i +  ": " + bugsEncontrados + " Bugs foram encontrados" + '<br>';
 
-    
+}
+
 }
