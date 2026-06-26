b00b4863b1306062ecc2dfb6ea97eb931e35d6dd
gustavocastilhos <gusmarcast@gmail.com>
2026-06-25 21:49:09 -0300

    Feat: Finalizando o relatorio de bugs

--- 04repeticao/relatorioDeBugs/script.js
+++ 04repeticao/relatorioDeBugs/script.js
@@ -1,10 +1,31 @@
 function relatorio(){
 let diasDeSprint = Number(prompt("Quantos dias tem o sprint? "))
-
+ let totalDeBugs = 0
+ let diaComMaisbugs = 0
+ let diaComMenosbugs = 1000000
+let queDiaMais = 0
+let queDiaMenos
 for(let i = 1; i <= diasDeSprint; i++){
     let bugsEncontrados = Number(prompt("Quantos bugs foram encontrados? "))
-    document.getElementById('listaDosDias').innerHTML += "Dia " + i +  ": " + bugsEncontrados + " Bugs foram encontrados" + '<br>';
+
+    if(bugsEncontrados > diaComMaisbugs){
+        diaComMaisbugs = bugsEncontrados
+        queDiaMais = i
+    }
+    if(diaComMenosbugs > bugsEncontrados){
+        diaComMenosbugs = bugsEncontrados
+        queDiaMenos = i
+    }
+
+    totalDeBugs += bugsEncontrados
+    let bugsPorDia = totalDeBugs / diasDeSprint
+    document.getElementById('listaDosBugs').innerHTML += "Dia " + i +  ": " + bugsEncontrados + " Bugs foram encontrados" + '<br>';
+    document.getElementById('totalDeBugs').innerHTML = 'Total de bugs encontrados: ' + totalDeBugs;
+    document.getElementById('BugsPorDia').innerHTML = 'Media de bugs por dia: ' + bugsPorDia;   
+    document.getElementById('DiaComMaisBugs').innerHTML = 'O dia com mais bugs foi o dia '+queDiaMais+" com um total de "+diaComMaisbugs+" bugs";   
+    document.getElementById('DiaComMenosBugs').innerHTML = 'O dia com menos bugs foi o dia '+queDiaMenos+" com um total de "+diaComMenosbugs+" bugs";   
+
+
 
 }
-
 }
