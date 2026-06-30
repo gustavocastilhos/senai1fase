a763e3def612f766cc67392b83cd5f92ddf400ac
gustavocastilhos <gusmarcast@gmail.com>
2026-06-30 19:22:57 -0300

    Feat: Firts arrays

---
+++ 05array/script.js
@@ -0,0 +1,17 @@
+const nomes = [
+  "Lúcio Fernando",
+  "Rivaldo Jesus",
+  "Mônica",
+  "Capitão Ganso",
+  "Peba",
+  "Gill Bates",
+  "Dona Bete",
+  "Kowalski",
+  "Heitor Tuga",
+  "Junin",
+  "Padre Ernan Buco",
+  "GENéZio",
+  "Bilu",
+  "Waldisney",
+  "Tião"
+];
