1f060308f93c1cee228e7b2baa06a6018ed3d3f9
gustavocastilhos <gusmarcast@gmail.com>
2026-07-14 21:45:39 -0300

    Feat: Adding some arrays and objects

--- 06crud/script.js
+++ 06crud/script.js
@@ -1,7 +1,38 @@
-let dino = {}
+let dinos = []
+
+
+
+function cadastrarDino(){
+    const novoDino = {
+        id: Date.now(),
+        nome: document.getElementById("input-nome").value,
+        altura: Number(document.getElementById("input-altura").value),
+        cor: document.getElementById("input-cor").value,
+        custo: Number(document.getElementById("input-custo").value),
+    }
+    dinos.push(novoDino)
+    console.log(dinos)
+    limparFormulario()
+}
+
+function limparFormulario(){
+        document.getElementById("input-nome").value = ""
+        document.getElementById("input-altura").value = ""
+        document.getElementById("input-cor").value = ""
+        document.getElementById("input-custo").value =""
+
+        document.getElementById("input-nome").focus()
+
+}
+
+
+
+
+
+
 
 function dino(){
-    let dino = {
+    let novoDino = {
     nome: "Velocirraptor",
     altura: 3,
     cor: "marrom bombom",
@@ -10,5 +41,3 @@ function dino(){
 }
 
 }
-
-console.log(dino);
